bd0b31171571de949538e5b59b6cc565d8cb5d0b
handsomedonkey <33711947+handsomedonkey@users.noreply.github.com>
2022-07-05 13:35:48 +0500

    Refactoring + Implementation of Queue, Hashmap, and PQ

--- Golang/package.go
+++ Golang/package.go
@@ -4,31 +4,33 @@ import (
 	"fmt"
 )
 
+
 // =========================================================
 // ===================== Linked List =======================
 // =========================================================
 
-type node[T any] struct {
+type LLNode[T any] struct {
 	val T
-	nxt *node[T]	//next node
-	prv *node[T]	//previous node
+	nxt *LLNode[T]	//next LLNode
+	prv *LLNode[T]	//previous LLNode
 }
+
 type LinkedList[T any] struct{
-	head *node[T]	//For remembering where to start traversing form
-	tail *node[T]	//For appending nodes to the tail
-	idx *node[T]	//Index node for traversing
+	head *LLNode[T]	//For remembering where to start traversing form
+	tail *LLNode[T]	//For appending LLNodes to the tail
+	idx *LLNode[T]	//Index LLNode for traversing
 	size int
 	Traversed bool
 }
 
 
 func (ll *LinkedList[T]) Push(input T) {
-	alc := &node[T]{val: input, prv:ll.tail}	//allocate mem for new node
+	alc := &LLNode[T]{val: input, prv:ll.tail}	//allocate mem for new node
 	if ll.head == nil {
 		ll.head = alc
 		ll.tail = ll.head
 	} else {
-		(*ll.tail).nxt = alc
+		ll.tail.nxt = alc
 		ll.tail = alc
 	}
 	ll.size++
@@ -38,26 +40,21 @@ func (ll *LinkedList[T]) Pop() T{
 	if(ll.size == 0){
 		panic("Cannot pop from empty linked list")
 	}
-	elm := (*ll.tail).val
+	elm := ll.tail.val
 	if ll.size == 1 {
 		ll.head = nil
 		ll.tail = nil
 	} else{
 		toPop := ll.tail
 		ll.tail = ll.tail.prv
-		(*(*toPop).prv).nxt = nil
+		toPop.prv.nxt = nil
 	}
 	ll.size --
 	return elm
-	// toPop, elm := ll.tail, (*ll.tail).val
-	// ll.tail = ll.tail.prv
-	// (*(*toPop).prv).nxt = nil
-	// ll.size --
-	// return elm
 }
 
 func (ll *LinkedList[T]) Shift(input T) {
-	alc := &node[T]{val: input, nxt:ll.head}	//allocate mem for new node
+	alc := &LLNode[T]{val: input, nxt:ll.head}	//allocate mem for new node
 	if ll.head == nil {
 		ll.head = alc
 		ll.tail = ll.head
@@ -72,20 +69,21 @@ func (ll *LinkedList[T]) Slice() T{
 	if(ll.size == 0){
 		panic("Cannot Slice from empty linked list")
 	}
-	elm := (*ll.head).val
+	elm := ll.head.val
 	if ll.size == 1 {
 		ll.head = nil
 		ll.tail = nil
 	} else{
 		toPop := ll.head
 		ll.head = ll.head.nxt
-		(*(*toPop).nxt).prv = nil
+		toPop.nxt.prv = nil
 	}
 	ll.size --
 	return elm
 }
 
 func (ll *LinkedList[T]) Display(params ...string){	//utility func for displaying LL
+	//TODO make optional paramater thingy better
 	if(ll.size == 0){
 		return
 	}
@@ -104,14 +102,14 @@ func (ll *LinkedList[T]) InitTraverse() T{
 	}
 	ll.Traversed = false
 	ll.idx = ll.head
-	return (*ll.idx).val
+	return ll.idx.val
 }
 
 func (ll *LinkedList[T]) Traverse() T{
 	var x T
-	if((*ll.idx).nxt!=nil){
-		ll.idx = (*ll.idx).nxt
-		x = (*ll.idx).val
+	if(ll.idx.nxt!=nil){
+		ll.idx = ll.idx.nxt
+		x = ll.idx.val
 	}else{
 		ll.Traversed = true
 	}
@@ -124,14 +122,14 @@ func (ll *LinkedList[T]) InitReverseTraverse() T{
 	}
 	ll.Traversed = false
 	ll.idx = ll.tail
-	return (*ll.idx).val
+	return ll.idx.val
 }
 
 func (ll *LinkedList[T]) ReverseTraverse() T{
 	var x T
-	if((*ll.idx).prv!=nil){
-		ll.idx = (*ll.idx).prv
-		x = (*ll.idx).val
+	if(ll.idx.prv!=nil){
+		ll.idx = ll.idx.prv
+		x = ll.idx.val
 	}else{
 		ll.Traversed = true
 	}
@@ -144,7 +142,7 @@ func (ll *LinkedList[T]) ReverseTraverse() T{
 // =========================================================
 
 
-type Stack[T any] struct {	//Built on top of Linked List class
+type Stack[T any] struct {	//Built using Linked List class
 	lst LinkedList[T]
 }
 
@@ -152,7 +150,7 @@ func (stack *Stack[T]) Peek() T {
 	if stack.lst.size == 0 {
 		panic("Cannot Peek empty stack")
 	}
-	return (*stack.lst.tail).val
+	return stack.lst.tail.val
 }
 
 func (stack *Stack[T]) Push(elm T) {
@@ -163,6 +161,83 @@ func (stack *Stack[T]) Pop() (elm T) {
 	return stack.lst.Pop()
 }
 
+// =========================================================
+// ======================== Queue ==========================
+// =========================================================
+
+
+type Queue[T any] struct {	//Built using Linked List class
+	lst LinkedList[T]
+}
+
+func (queue *Queue[T]) Peek() T {
+	if queue.lst.size == 0 {
+		panic("Cannot Peek empty stack")
+	}
+	return queue.lst.head.val
+}
+
+func (queue *Queue[T]) Push(elm T) {
+	queue.lst.Push(elm)
+}
+
+func (queue *Queue[T]) Pop() (elm T) {
+	return queue.lst.Slice()
+}
+
+
+// =========================================================
+// ======================= Hashmap =========================
+// =========================================================
+
+//Built-in Implementation with 'map'
+
+
+
+// =========================================================
+// =================== Priority Queue ======================
+// =========================================================
+
+//TODO finish implementation
+
+type PQNode[T any] struct{
+	val int
+	data T
+	parent *PQNode[T]
+	left *PQNode[T]
+	right *PQNode[T]
+}
+
+type PriorityQueue[T any] struct{
+	root *PQNode[T]
+	tail *PQNode[T]
+	size int
+}
+
+func (pq *PriorityQueue[T]) Peek() T{
+	return pq.root.data
+}
+
+func (pq *PriorityQueue[T]) Push(elm T, priority int){
+	alc := &PQNode[T]{data: elm, parent: pq.tail, val: priority}
+	if pq.root == nil{
+		pq.root = alc
+		pq.tail = alc
+	}else{
+		if(pq.size%2 == 0){
+			pq.tail.left = alc
+		}else{
+			pq.tail.right = alc
+		}
+		pq.tail = alc
+	}
+}
+
+// func (pq *PriorityQueue[]) Display(){
+// }
+
+// func InitTraverse(pq *PriorityQueue[]) 
+
 
 // =========================================================
 // ======================== Test ===========================
@@ -170,12 +245,7 @@ func (stack *Stack[T]) Pop() (elm T) {
 
 
 func main(){
-	lst := LinkedList[int]{}
-	lst.Push(1)
-	lst.Push(2)
-	lst.Push(3)
-	lst.Pop()
-	lst.Pop()
-	lst.Pop()
-	lst.Display()
+	pq := PriorityQueue[string]{}
+	pq.Push("Hello", 1)
+	pq.Push("World", 2)
 }
